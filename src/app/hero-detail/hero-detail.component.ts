60f7176444e3642bc6a1a8b5755b76b89cfb8c34
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-05-30 11:58:08 -0300

    6. Get Data From a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -32,4 +32,11 @@ export class HeroDetailComponent {
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
+  
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
